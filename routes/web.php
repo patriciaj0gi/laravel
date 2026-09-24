0455454179e7533560f8e46d5cca45534947ba01
patriciaj0gi <patricia.jogi@ametikool.ee>
2026-09-24 10:01:28 +0300

    what you changed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Models\Author;
+use App\Models\Book;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tere', function () {
+
+    $book = Book::find(1);
+
+    $authorBooks = Author::withCount('books')->get();
+
+    $author = Author::find(1);
+
+    $authorBooks = $author->books;
+
+    $bookAuthor = $book->author;
+
+    $authorWithBooks = Author::with('books')->get();
+
+    return $authorWithBooks;
+});
+
+
