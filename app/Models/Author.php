daff701ae0786d3f6547111f42ba0669454278ef
Ralf Heinsoo <ralf@veebisepad.ee>
2026-09-24 08:36:24 +0300

    feat: add Author and Book models with factories and migrations; update DatabaseSeeder

---
+++ app/Models/Author.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\AuthorFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    /** @use HasFactory<AuthorFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+}
